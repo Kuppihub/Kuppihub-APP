6c4f17ee16651cd679ceaa1fd626c68c4ff67362
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-12-29 11:59:12 +0530

    init:kuppihub compose app

---
+++ composeApp/src/commonMain/kotlin/org/kuppihub/app/Platform.kt
@@ -0,0 +1,7 @@
+package org.kuppihub.app
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
---
+++ composeApp/src/jsMain/kotlin/org/kuppihub/app/Platform.js.kt
@@ -0,0 +1,7 @@
+package org.kuppihub.app
+
+class JsPlatform: Platform {
+    override val name: String = "Web with Kotlin/JS"
+}
+
+actual fun getPlatform(): Platform = JsPlatform()
