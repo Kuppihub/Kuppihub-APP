ffcd872a4019b327da7a20277429a854fe082f77
NipunSGeeTH <sangeethnipun385@gmail.com>
2026-01-08 20:21:08 +0530

    fix:dashbord button route

--- composeApp/src/commonMain/kotlin/org/kuppihub/app/ui/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/org/kuppihub/app/ui/MainScreen.kt
@@ -79,8 +79,16 @@ fun MainScreen(
                         ),
                         onClick = {
                             navController.navigate(tab.route) {
-                                popUpTo(navController.graph.findStartDestination().id) { saveState = true }
+                                // Pop up to the start destination of the graph to
+                                // avoid building up a large stack of destinations
+                                // on the back stack as users select items
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                // Avoid multiple copies of the same destination when
+                                // reselecting the same item
                                 launchSingleTop = true
+                                // Restore state when reselecting a previously selected item
                                 restoreState = true
                             }
                         }
